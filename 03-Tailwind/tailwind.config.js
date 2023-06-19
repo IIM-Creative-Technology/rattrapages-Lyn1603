9a4fe7537da33489715323c49126f952ad52b614
Lynda Benabdessadok <lylyben74@gmail.com>
2023-06-19 22:37:33 +0200

    begin desktop integration

---
+++ 03-Tailwind/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,png,jpg,jpeg}"],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
